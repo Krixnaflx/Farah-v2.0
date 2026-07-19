a88f27aefa17f3eed96d3ecbd3d26daff7f38aea
Krishna <krixdurex@gmail.com>
2026-07-19 10:18:57 +0530

    Update infoemoji.js

--- emojis/infoemoji.js
+++ emojis/infoemoji.js
@@ -3,13 +3,13 @@
 //  Emojis used by info commands
 // ============================================================
 module.exports = {
-  bot: "<:vm_bot:1496157518521434132>",
-  invite: "<:vm_invite:1496157523953057844>",
-  support: "<:vm_support:1496157527950495887>",
-  server: "<:vm_server:1496157532241002739>",
-  shield: "<:vm_shield:1496157536775311400>",
-  command: "📜",
-  star: "<:vm_star:1496157541166612522>",
-  team:    "👥",
-  help:    "🧰",
+  bot: "<a:pepe_confused:1525964303557984336>",
+  invite: "<:links:1526066522521341962>",
+  support: "<:discord:1528074869814726807>",
+  server: "<:PartnerServer:1528244539935035412>",
+  shield: "<:cat_antinuke:1525842918030708756>",
+  command: "<:clipboard:1525842972812247130>",
+  star: "<:icon_23:1490275583865913346>",
+  team:    "<:icon_21:1490275566220218419>",
+  help:    "<:vm_slap:1528157704852340787>",
 };
